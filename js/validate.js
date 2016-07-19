a78c419522f7627e48d9611ee3410d07cd73c520
liuyawin <717200899@qq.com>
2016-07-19 21:23:20 +0800

    requirejs测试项目

---
+++ js/validate.js
@@ -0,0 +1,15 @@
+define(['jquery'],function($){
+
+	return {
+		isEmpty: function() {
+			
+		},
+		checkLength: function() {
+			
+		},
+		isEqual: function(str1,str2) {
+			return str1 === str2;
+		}
+		
+	}
+})
