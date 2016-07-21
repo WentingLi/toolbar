21ce9f0fd3ce865cf3b56207c99d64467798bbff
liuyawin <717200899@qq.com>
2016-07-21 11:26:19 +0800

    ！模块化

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
-requirejs.config({
+requirejs.config({//引入jQuery模块，为其定义别名
 	paths: {
-		jquery: 'jquery.min'
+		jquery: 'jquery.min'//不要.js后缀
 	}
-});//引入jQuery模块，为其定义别名
+});
 /**
  * function为回调函数，它有一个参数为传入的模块，这里命名为$
  */
-requirejs(['jquery'],function($){
-});
+requirejs(['jquery','backtop'],function($,backtop){
+	var backtop = new backtop.BackTop( $('#backTop'),{
+		mode: 'move',
+		pos: 100,
+		speed: 2000
+	});
+	
+	/*$('#backTop').on('click',$.proxy(scroll.move,scroll));//proxy方法用于调整this的指向
+	
+	$(window).on('scroll',function() {
+		checkPosition($(window).height());
+	});
+	
+	checkPosition($(window).height());
 
-requirejs(['jquery','validate'],function($,validate){
-	console.log(validate.isEqual(1,1));
+	function checkPosition(pos){
+		if($(window).scrollTop()>pos)
+			$('#backTop').fadeIn();
+		else {
+			$('#backTop').fadeOut();
+		}
+	}*/
 });
